41ee588d5c1a952a1a06ecc9e6308adee8f1e70b
anjanajs006 <77201934+anjanajs006@users.noreply.github.com>
2023-05-18 19:52:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
+
 function validation(){
+let x=document.getElementById("name-input").value;
+let y=document.getElementById("email-input").value;
+let z=document.getElementById("message-input").value;
 
-    let x=document.getElementById("name-input").value;
-    let y=document.getElementById("email-input").value;
-    let z=document.getElementById("message-input").value;
+if(x=="")
+{
+    document.getElementById("name-validation").style.display="block"
     
-    if(x=="")
-    {
-        document.getElementById("name-validation").style.display="block"
-        
-    }
-
-    
-    if(y=="")
-    {
-        document.getElementById("email-validation").style.display="block"
-        
-    }
-
-    
-    if(e=="")
-    {
-        document.getElementById("message-validation").style.display="block"
-        
-    }
-    
-    if((x || y || z)=="")
-    {
-        return false;
-    }
 }
+
+
+if(y=="")
+{
+    document.getElementById("email-validation").style.display="block"
+    
+}
+
+
+if(e=="")
+{
+    document.getElementById("message-validation").style.display="block"
+    
+}
+
+if((x || y || z)=="")
+
+
+return false;
+}
+
